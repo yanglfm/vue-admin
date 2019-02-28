9aceea0fd296d7dcbd3c2a2de8b37ed707014b93
seventeenice <2410913458@qq.com>
2019-02-28 20:48:40 +0800

    Initial commit

---
+++ src/api/login.js
@@ -0,0 +1,35 @@
+import request from '@/utils/request';
+
+export function login(username, password) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  });
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  });
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  });
+}
+
+export function selectAll(data) {
+  return request({
+    url: '/test/selectAll',
+    method: 'post',
+    data: data
+  });
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name:{{ name }}</div>
+    <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <el-button type="primary" @click="getAll">测试按钮</el-button>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name',
+        'roles'
+      ])
+    },
+    methods: {
+      getAll() {
+        this.$store.dispatch('Login', this.loginForm).then(() => {
+          this.loading = false;
+          this.$router.push({ path: this.redirect || '/' });
+        }).catch(() => {
+          this.loading = false;
+        });
+      }
+    }
+  };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
+  }
+</style>
